d5a9ff45cab72a0375311d73269d81eda92bc0ff
SamP-S <sam.ps1@outlook.com>
2024-03-13 20:30:50 +0000

    fix run script addons

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#"/bin/zsch"
+#"/bin/zsh"
 SCRIPT="setup_brick.py"
 SCRIPT_DIR="${PWD}/$(dirname ${BASH_SOURCE[0]})"
 SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT"
 
-blender --background --python $SCRIPT_PATH
-# blender --background --python-console
+blender --background --addons io_scene_importldraw --python $SCRIPT_PATH
+# blender --background --addons io_scene_importldraw --python-console
